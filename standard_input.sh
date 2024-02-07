f8a47df3faaa24563ed9c65ef45c2e72c61cca46
Daniel Ekholm <daniel.ekholm.it@gmail.com>
2024-02-07 19:07:45 +0100

    added datastreams to updater for better error handling and logging

---
+++ standard_input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Standard input example, alltså text som kommer IN.
+
+echo "Please enter your name:"
+read myname
+
+echo "your name is: $myname"
+
--- update.sh
+++ update.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
 release_file=/etc/os-release
+logfile=/var/log/updater.log
+errorlog=/var/log/updater_errors.log
 
 if grep -q "Arch" $release_file # -q står för quiet mdoe
 then
-    sudo pacman -Syu
+    sudo pacman -Syu 1>>$logfile 2>>$errorlog
+    if [ $? -ne 0 ]
+    then
+        echo "An error occured, please check the $errorlog file."
+    fi
 fi
 
 # Här letar vi efter Ubuntu/Debian/Kali
 if grep -q "Ubuntu\|Debian\|Kali" $release_file
 then
-    sudo apt update # uppdatera packet index
-    sudo apt dist-upgrade -y
+    sudo apt update 1>>$logfile 2>>$errorlog
+    if [ $? -ne 0 ]
+    then
+        echo "An error occured, please check the $errorlog file."
+    fi
+    sudo apt dist-upgrade -y 1>>$logfile 2>>$errorlog
     echo -e "#########################\n"
-    
-    echo "uppdaterad och klar, exit code: $?"
+    echo -e "Uppdateringen lyckades, exit code: $?"
+    if [ $? -ne 0 ]
+    then
+        echo "An error occured, please check the $errorlog file."
+    fi
 fi
