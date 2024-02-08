c022d75c595ff55ad195bc23e306d4c2c698a43f
Daniel Ekholm <daniel.ekholm.it@gmail.com>
2024-02-08 19:41:38 +0100

    added functions to update script

--- update.sh
+++ update.sh
@@ -4,28 +4,24 @@ release_file=/etc/os-release
 logfile=/var/log/updater.log
 errorlog=/var/log/updater_errors.log
 
-if grep -q "Arch" $release_file # -q står för quiet mdoe
-then
-    sudo pacman -Syu 1>>$logfile 2>>$errorlog
+check_exit_status() {
     if [ $? -ne 0 ]
     then
         echo "An error occured, please check the $errorlog file."
     fi
+}
+
+if grep -q "Arch" $release_file # -q står för quiet mdoe
+then
+    sudo pacman -Syu 1>>$logfile 2>>$errorlog
+    check_exit_status    
 fi
 
 # Här letar vi efter Ubuntu/Debian/Kali
 if grep -q "Ubuntu\|Debian\|Kali" $release_file
 then
     sudo apt update 1>>$logfile 2>>$errorlog
-    if [ $? -ne 0 ]
-    then
-        echo "An error occured, please check the $errorlog file."
-    fi
+    check_exit_status
     sudo apt dist-upgrade -y 1>>$logfile 2>>$errorlog
-    echo -e "#########################\n"
-    echo -e "Uppdateringen lyckades, exit code: $?"
-    if [ $? -ne 0 ]
-    then
-        echo "An error occured, please check the $errorlog file."
-    fi
+    check_exit_status
 fi
